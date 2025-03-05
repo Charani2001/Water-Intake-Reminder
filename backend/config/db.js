d21198d7a44f4160405f1df960ab52472229582c
Charani2001 <charanisubasinghe10@gmail.com>
2025-03-05 19:37:25 +0530

    edit db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,12 +3,15 @@ require("dotenv").config();
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(` MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error(" MongoDB Connection Error:", error);
-        process.exit(1);
+      await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log("MongoDB Connected!");
+    } catch (err) {
+      console.error("MongoDB Connection Error:", err);
+      process.exit(1);
     }
-};
+  };
 
 module.exports = connectDB;
