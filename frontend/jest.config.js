d41d0e4836bfb2425f011b45611dd6043bee7ba8
Charani2001 <charanisubasinghe10@gmail.com>
2025-03-16 19:11:56 +0530

    update

---
+++ frontend/jest.config.js
@@ -0,0 +1,18 @@
+export default {
+    testEnvironment: "jsdom",
+    transform: {
+      "^.+\\.(js|jsx)$": "babel-jest"
+    },
+    transformIgnorePatterns: [
+      "node_modules/(?!(axios)/)"
+    ],
+    moduleNameMapper: {
+      "^axios$": "<rootDir>/node_modules/axios/index.js"
+    },
+    testMatch: [
+      "**/src/**/*.test.js",
+      "**/src/**/*.spec.js",
+      "**/src/**/*.{test,spec}.{js,jsx,ts,tsx}"
+    ]
+  };
+  
